b2866bf89d8daddd362ec60ce4d4cc52f57a18f9
Costa-Vitor-Fernandes <costa.vitor.fernandes@gmail.com>
2026-01-30 13:28:06 -0300

    fix: changes a format on script

--- scripts/ci/calculate-version.js
+++ scripts/ci/calculate-version.js
@@ -9,7 +9,7 @@ const baseVersion = process.env.BASE_VERSION || '0.0.1';
 
 function getCommitsFromTag() {
   try {
-    const commits = execSync(`git log ${baseTag}..HEAD --format=%H||%s||%b####`, { encoding: 'utf-8' })
+    const commits = execSync(`git log ${baseTag}..HEAD --format=%H%n%s%n%b%n---END---`, { encoding: 'utf-8' })
       .trim()
       .split('####')
       .filter(Boolean)
