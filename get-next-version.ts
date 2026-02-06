b744df34de5bdc2244ebbbe8c4a94ac533b9351b
Costa-Vitor-Fernandes <costa.vitor.fernandes@gmail.com>
2026-02-06 13:16:44 -0300

    ci: this file gets a new flag --releaseType

--- get-next-version.ts
+++ get-next-version.ts
@@ -4,26 +4,30 @@
  * Get the next version based on the conventional commits.
  * @param currentVersion - The current version.
  * @returns The next version.
+ * @param releaseType - The type of release: 'major', 'minor', or 'patch'. Optional.
+ * @returns The release type.
  */
-// USAGE: bun scripts/get-next-version.ts <currentVersion>
+// USAGE: bun scripts/get-next-version.ts <currentVersion> <releaseType>
 
 
 import { Bumper } from 'conventional-recommended-bump'
 import { inc } from 'semver'
 
-const currentVersion = Bun.argv[2]
-if (!currentVersion) {
-  console.error('Current version is required')
-  process.exit(1)
+const args = Bun.argv.slice(2);
+console.log('Args:', args);
+const currentVersion = args[0];
+const isReleaseTypeRequested = args.includes('--releaseType');
+
+if (!currentVersion || currentVersion.startsWith('--')) {
+  console.error('Usage: bun get-next-version.ts <currentVersion> [--releaseType]');
+  process.exit(1);
 }
 const bumper = new Bumper().loadPreset('conventionalcommits')
 const recommendation = await bumper.bump()
 //@ts-ignore
 const releaseType = recommendation.releaseType
-
-// setOutput('current', currentVersion)
-// setOutput('type', releaseType)
 const nextVersion = inc(currentVersion, releaseType)
-// setOutput('next', nextVersion)
-console.log(nextVersion)
 
+isReleaseTypeRequested
+  ? console.log(releaseType)
+  : console.log(nextVersion);
