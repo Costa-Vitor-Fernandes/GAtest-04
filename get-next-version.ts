eddc4a26ed7cb9822e651693716430e27a36bb0b
Costa-Vitor-Fernandes <costa.vitor.fernandes@gmail.com>
2026-02-06 13:51:11 -0300

    ci: solve init

--- get-next-version.ts
+++ get-next-version.ts
@@ -7,14 +7,14 @@
  * @param releaseType - The type of release: 'major', 'minor', or 'patch'. Optional.
  * @returns The release type.
  */
-// USAGE: bun scripts/get-next-version.ts <currentVersion> <releaseType>
+// USAGE: bun scripts/get-next-version.ts <currentVersion> [--releaseType]
 
 
 import { Bumper } from 'conventional-recommended-bump'
 import { inc } from 'semver'
 
 const args = Bun.argv.slice(2);
-console.log('Args:', args);
+// console.log('Args:', args);
 const currentVersion = args[0];
 const isReleaseTypeRequested = args.includes('--releaseType');
 
@@ -22,9 +22,9 @@ if (!currentVersion || currentVersion.startsWith('--')) {
   console.error('Usage: bun get-next-version.ts <currentVersion> [--releaseType]');
   process.exit(1);
 }
+
 const bumper = new Bumper().loadPreset('conventionalcommits')
 const recommendation = await bumper.bump()
-//@ts-ignore
 const releaseType = recommendation.releaseType
 const nextVersion = inc(currentVersion, releaseType)
 
